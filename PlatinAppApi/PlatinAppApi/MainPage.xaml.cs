05294b00b043a5b3682254869265b7ac937c3846
paulookino <paulookino@hotmail.com>
2017-12-26 11:41:41 -0200

    View, Model e Repositório

--- PlatinAppApi/PlatinAppApi/MainPage.xaml.cs
+++ PlatinAppApi/PlatinAppApi/MainPage.xaml.cs
@@ -1,17 +1,130 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xamarin.Forms;
+using PlatinAppApi.Models;
+using PlatinAppApi.Service;
 
 namespace PlatinAppApi
 {
-	public partial class MainPage : ContentPage
-	{
-		public MainPage()
-		{
-			InitializeComponent();
-		}
-	}
+    public partial class MainPage : ContentPage
+    {
+        DataService dataService;
+        List<PrdCab> produtos;
+
+        public MainPage()
+        {
+            
+            InitializeComponent();
+            dataService = new DataService();
+            AtualizaDados();
+        }
+
+        private async void btnAdicionar_Clicked(object sender, EventArgs e)
+        {
+            if (Valida())
+            {
+                PrdCab novoProduto = new PrdCab
+                {
+                    Des = txtDes.Text.Trim(),
+                    //Categoria = txtCategoria.Text.Trim(),
+                    //Preco = Convert.ToDecimal(txtPreco.Text)
+                };
+
+                try
+                {
+                    await dataService.AddProdutoAsync(novoProduto);
+                    LimpaProduto();
+                    AtualizaDados();
+                }
+                catch (Exception ex)
+                {
+                    await DisplayAlert("Erro", ex.Message, "OK");
+                }
+            }
+            else
+            {
+                await DisplayAlert("Erro", "Dados inválidos...", "OK");
+            }
+        }
+
+        async void AtualizaDados()
+        {
+            produtos = await dataService.GetProdutosAsync();
+            listaProdutos.ItemsSource = produtos.OrderBy(item => item.Des).ToList();
+        }
+
+        private async void OnAtualizar(object sender, EventArgs e)
+        {
+            if (Valida())
+            {
+                try
+                {
+                    var mi = ((MenuItem)sender);
+                    PrdCab produtoAtualizar = (PrdCab)mi.CommandParameter;
+
+                    produtoAtualizar.Des = txtDes.Text;
+                    //produtoAtualizar.Categoria = txtCategoria.Text;
+                    //produtoAtualizar.Preco = Convert.ToDecimal(txtPreco.Text);
+
+                    await dataService.UpdateProdutoAsync(produtoAtualizar);
+
+                    LimpaProduto();
+                    AtualizaDados();
+                }
+                catch (Exception ex)
+                {
+                    await DisplayAlert("Erro", ex.Message, "OK");
+                }
+            }
+            else
+            {
+                await DisplayAlert("Erro", "Dados inválidos...", "OK");
+            }
+        }
+
+        private async void OnDeletar(object sender, EventArgs e)
+        {
+            try
+            {
+                var mi = ((MenuItem)sender);
+                PrdCab produtoDeletar = (PrdCab)mi.CommandParameter;
+                await dataService.DeletaProdutoAsync(produtoDeletar);
+                LimpaProduto();
+                AtualizaDados();
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Erro", ex.Message, "OK");
+            }
+        }
+
+        private void listaProdutos_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var produto = e.SelectedItem as PrdCab;
+            txtDes.Text = produto.Des;
+            //txtCategoria.Text = produto.Categoria;
+            //txtPreco.Text = produto.Preco.ToString();
+        }
+
+        private void LimpaProduto()
+        {
+            txtDes.Text = "";
+            //txtCategoria.Text = "";
+            //txtPreco.Text = "";
+        }
+
+        private bool Valida()
+        {
+            //if (string.IsNullOrEmpty(txtNome.Text) && string.IsNullOrEmpty(txtCategoria.Text) && string.IsNullOrEmpty(txtPreco.Text))
+            if (string.IsNullOrEmpty(txtDes.Text))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
 }
---
+++ PlatinAppApi/PlatinAppApi/Service/DataService.cs
@@ -0,0 +1,87 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using PlatinAppApi.Models;
+
+namespace PlatinAppApi.Service
+{
+    public class DataService
+    {
+        HttpClient client = new HttpClient();
+
+        public async Task<List<PrdCab>> GetProdutosAsync()
+        {
+            try
+            {
+                string url = "http://localhost:49807/api/prdcab/listar";
+                var response = await client.GetStringAsync(url);
+                var produtos = JsonConvert.DeserializeObject<List<PrdCab>>(response);
+                return produtos;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task AddProdutoAsync(PrdCab produto)
+        {
+            try
+            {
+                
+
+                string url = "http://localhost:49807/api/prdcab/inserir/{0}";
+
+                var uri = new Uri(string.Format(url, produto.Ide));
+
+                var data = JsonConvert.SerializeObject(produto);
+                var content = new StringContent(data, Encoding.UTF8, "application/json");
+
+                HttpResponseMessage response = null;
+
+                response = await client.PostAsync(uri, content);
+                if (!response.IsSuccessStatusCode)
+                {
+                    throw new Exception("Erro ao incluir produto");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task UpdateProdutoAsync(PrdCab produto)
+        {
+            string url = "http://localhost:49807/api/prdcab/editar/{0}";
+            var uri = new Uri(string.Format(url, produto.Ide));
+
+            var data = JsonConvert.SerializeObject(produto);
+            var content = new StringContent(data, Encoding.UTF8, "application/json");
+
+            HttpResponseMessage response = null;
+            response = await client.PutAsync(uri, content);
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception("Erro ao atualizar produto");
+            }
+
+        }
+
+        public async Task DeletaProdutoPorIndiceAsync(int indice)
+        {
+            string url = "http://localhost:49807/api/prdcab/excluir/{0}";
+            await client.DeleteAsync(string.Concat(url, indice));
+        }
+
+        public async Task DeletaProdutoAsync(PrdCab produto)
+        {
+            string url = "http://localhost:49807/api/prdcab/excluir/{0}";
+            var uri = new Uri(string.Format(url, produto.Ide));
+            await client.DeleteAsync(uri);
+        }
+    }
+}
