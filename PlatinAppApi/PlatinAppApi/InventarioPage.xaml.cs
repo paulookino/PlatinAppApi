14b1211cf932ea8ba392ee1d6498bdaf7511f7f5
paulookino <paulookino@hotmail.com>
2018-01-03 12:40:59 -0200

    Capturar QRCode

--- PlatinAppApi/PlatinAppApi/InventarioPage.xaml.cs
+++ PlatinAppApi/PlatinAppApi/InventarioPage.xaml.cs
@@ -41,9 +41,7 @@ namespace PlatinAppApi
         bool exibindoMsg = false;
 
         ZXingScannerPage scanPage = null;
-
         
-
         public async Task Capturar()
         {
             scanPage = await Util.CapturarCodigoAsync(scanPage, "Escanear Codigo", ZXing.BarcodeFormat.EAN_13);// QR_CODE);
@@ -105,9 +103,75 @@ namespace PlatinAppApi
             }
         }
 
+        public async Task CapturarQRCode()
+        {
+            scanPage = await Util.CapturarCodigoAsync(scanPage, "Escanear Codigo", ZXing.BarcodeFormat.QR_CODE);// QR_CODE);
+
+            try
+            {
+                if (!iniCapt)
+                {
+                    scanPage.OnScanResult += (resut) =>
+                    {
+                        Device.BeginInvokeOnMainThread(async () =>
+                        {
+                            try
+                            {
+                                if (exibindoMsg)
+                                {
+                                    return;
+                                }
+
+                                scanPage.IsScanning = false;
+
+                                if (!string.IsNullOrEmpty(CodigoCapturado))
+                                {
+                                    return;
+                                }
+
+                                Util.Vibrar();
+
+                                CodigoCapturado = resut.Text;
+                                await Navigation.PopModalAsync();
+
+
+                                //Inventario inventario = new Inventario
+                                //{
+                                //    InvCodigo = CodigoCapturado
+                                //    //Categoria = txtCategoria.Text.Trim(),
+                                //    //Preco = Convert.ToDecimal(txtPreco.Text)
+                                //};
+
+                                //await dataService.AddInventarioAsync(inventario);
+                            }
+                            catch (Exception ex)
+                            {
+                                exibindoMsg = true;
+                                await this.DisplayAlert("Atenção", "Codigo invalido tente novamente !", "ok");
+                                exibindoMsg = false;
+                            }
+
+                        });
+                    };
+                    iniCapt = true;
+                }
+                CodigoCapturado = "";
+                await Navigation.PushModalAsync(scanPage);
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+
         private async void Button_Clicked(object sender, EventArgs e)
         {
             await Capturar();
+        }
+
+        private async void btnCapturaQrCode_Clicked(object sender, EventArgs e)
+        {
+            await CapturarQRCode();
         }
 
         async void Button_Clicked_1(object sender, EventArgs e)
@@ -115,6 +179,7 @@ namespace PlatinAppApi
             await Navigation.PushModalAsync(new VerCodigo());
 
         }
+
         async void btnTodosCodigos_Clicked(object sender, EventArgs e)
         {
             await Navigation.PushModalAsync(new TodosCodigos());
